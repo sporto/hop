2bba5653dbd836446d7c5c0f91969472b392fce5
Sebastian Porto <sebasporto@gmail.com>
2015-10-07 17:50:36 +1100

    Basic app

---
+++ App.elm
@@ -0,0 +1,53 @@
+module App where
+
+import Html as H
+
+import StartApp
+import Effects exposing (Effects, Never)
+import Html.Attributes exposing (href)
+
+type alias AppModel = {
+    }
+                      
+zeroModel: AppModel
+zeroModel =
+  {}
+
+type Action
+  = DoNothing
+
+init: (AppModel, Effects Action)
+init =
+  (zeroModel, Effects.none)
+
+update: Action -> AppModel -> (AppModel, Effects Action)
+update action model =
+  (model, Effects.none)
+
+view: Signal.Address Action -> AppModel -> H.Html
+view address model =
+  H.div [] [
+    H.text "Hello"
+    , menu address model
+  ]
+
+menu: Signal.Address Action -> AppModel -> H.Html
+menu address model =
+  H.div [] [
+    H.a [ href "#/users" ] [
+      H.text "Users"
+    ]
+  ]
+
+app =
+  StartApp.start {
+    init = init,
+    update = update,
+    view = view,
+    inputs = []
+  }
+
+main: Signal H.Html
+main =
+  app.html
+     
