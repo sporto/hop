ac866e25f8d018242aa79c3c5ea0597cf91c73d4
Sebastian Porto <sebasporto@gmail.com>
2016-03-18 08:05:19 +1100

    Test

--- support/prepare_test.sh
+++ support/prepare_test.sh
@@ -3,7 +3,15 @@ node --version
 npm --version
 npm install -g elm
 
+echo "============================"
+echo "Installing deps for test"
 cd ./test && npm install
 cd ./test && elm package install -y
+
+echo "============================"
+echo "Installing deps for basic app"
 cd ./examples/basic && elm package install -y
+
+echo "============================"
+echo "Installing deps for full app"
 cd ./examples/full && elm package install -y
