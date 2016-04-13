017cd161226464ecb53f71d89dcaeb7e128e674e
Sebastian Porto <sebasporto@gmail.com>
2016-04-13 13:59:24 +1000

    Add wait in tests

--- test/integration/examples/both/test/full/all_test.exs
+++ test/integration/examples/both/test/full/all_test.exs
@@ -10,6 +10,10 @@ defmodule BFullTest do
     find_element(:id, "title")
   end
 
+  defp wait do
+    :timer.sleep(200)
+  end
+
   def assert_title(title) do
     ele = find_title()
     assert inner_html(ele) == title
@@ -21,64 +25,77 @@ defmodule BFullTest do
   end
 
   test "it shows home" do
-    :timer.sleep(500)
+    wait()
 
     navigate_to("/app")
+    wait()
 
-    source = page_source()
-    IO.puts source
+    str = visible_page_text()
+    IO.puts str
 
     assert_title("Home")
   end
 
   test "it shows languages" do
-    :timer.sleep(500)
+    wait()
 
     navigate_to("/app/languages")
+    wait()
+
     assert_title("Languages")
   end
 
   test "it shows about" do
-    :timer.sleep(500)
+    wait()
 
     navigate_to("/app/about")
+    wait()
+
     assert_title("About")
   end
 
   test "it shows a language" do
-    :timer.sleep(500)
+    wait()
 
     navigate_to("/app/languages/1")
+    wait()
+
     ele = find_element(:id, "titleLanguage")
     assert inner_html(ele) == "Elm"
   end
 
   test "it shows the query" do
+    wait()
+
     navigate_to("/app/languages/1?typed=dynamic")
     # TODO
   end
 
   test "it changes the path" do
-    :timer.sleep(500)
+    wait()
 
     navigate_to("/app")
+    wait()
 
     assert_title("Home")
 
     click_btn("btnLanguages")
+    wait()
 
     actual = execute_script("return window.location.pathname")
     assert actual == "/app/languages"
   end
 
   test "it changes the query" do
-    :timer.sleep(500)
+    wait()
 
     navigate_to("/app/languages")
+    wait()
 
     assert_title("LanguagesFFFF")
 
     click_btn("btnDynamic")
+    wait()
 
     actual = execute_script("return window.location.search")
     assert actual == "?typed=dynamic"
