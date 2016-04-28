5aae84c84af32166045b8b5fc4bd6648567e683c
Sebastian Porto <sebasporto@gmail.com>
2016-04-28 17:53:06 +1000

    Add some comments

--- test/integration/examples/both/run.sh
+++ test/integration/examples/both/run.sh
@@ -3,15 +3,19 @@
 mix deps.get
 
 # Wait for Elm
+echo "Looking for $APP_HOST"
 until nc -z $APP_HOST $APP_PORT; do
   echo "Waiting for $APP_HOST ..."
   sleep 1
 done
+echo "Found $APP_HOST"
 
 # Wait for Webdriver
+echo "Looking for $WEBDRIVER_HOST"
 until nc -z $WEBDRIVER_HOST $WEBDRIVER_PORT; do
   echo "Waiting for $WEBDRIVER_HOST ..."
   sleep 1
 done
+echo "Found $WEBDRIVER_HOST"
 
 mix test test/$TEST_DIR
