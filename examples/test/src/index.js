3526c8423dd917706b32a32b9b9632a8e12984ab
Sebastian Porto <sebasporto@gmail.com>
2016-04-29 17:04:06 +1000

    Push config to Elm

--- examples/test/src/index.js
+++ examples/test/src/index.js
@@ -1,10 +1,20 @@
 'use strict';
 
+var config = window.CONFIG;
+
+if (config == null) {
+	throw new Error('Expected config');
+}
+
+if (config.hash == null) {
+	throw new Error('Expected config.hash');
+}
+
+if (config.basePath == null) {
+	throw new Error('Expected config.basePath');
+}
+
 var Elm = require('./Main.elm');
 var mountNode = document.getElementById('main');
 
-var config = {
-	basePath: '',
-	hash: true,
-}
 var app = Elm.embed(Elm.Main, mountNode, {config: config});
