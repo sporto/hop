202c19c9d5aa0c1340121cf85bdc8dddae0a5246
Sebastian Porto <sebasporto@gmail.com>
2016-04-29 17:01:41 +1000

    Move index to an Express template and load config

--- examples/test/dev_server.js
+++ examples/test/dev_server.js
@@ -1,14 +1,18 @@
 var path     = require('path');
 var express  = require('express');
-var http     = require('http');
-var webpack  = require('webpack');
-var config   = require('./webpack.config');
-var app      = express();
-var compiler = webpack(config);
-var host     = 'localhost';
-var port     = 9000;
-var hash     = process.env.CONFIG_HASH || false;
-var basePath = process.env.CONFIG_BASEPATH || '';
+var http      = require('http');
+var webpack   = require('webpack');
+var config    = require('./webpack.config');
+var app       = express();
+var compiler  = webpack(config);
+var host      = 'localhost';
+var port      = 9000;
+var basePath  = process.env.CONFIG_BASEPATH || '';
+var hash      = process.env.CONFIG_HASH || false;
+var appConfig = {
+	basePath: basePath,
+	hash: hash,
+} 
 
 app.use(require('webpack-dev-middleware')(compiler, {
 	// contentBase: 'src',
@@ -18,8 +22,12 @@ app.use(require('webpack-dev-middleware')(compiler, {
 	stats: { colors: true },
 }))
 
+// app.set('view engine', 'mustache')
+app.set('view engine', 'ejs');
+
 function respondWithIndex(req, res) {
-	res.sendFile(path.join(__dirname, 'public/index.html'));
+	// res.sendFile(path.join(__dirname, 'public/index.html'));
+	res.render('index.ejs', { config: appConfig});
 }
 
 app.get('/' + basePath, respondWithIndex);
